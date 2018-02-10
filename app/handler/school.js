4746aa9b7fcfb7be896412f90ceb55b6a36a915a
DSMdongly <gdh0608@naver.com>
2018-02-11 02:34:54 +0900

    fixed search logic yeeeii

--- app/handler/school.js
+++ app/handler/school.js
@@ -5,9 +5,13 @@ module.exports = {
     find: async ctx => {
         if (!ctx.user) ctx.throw(401);
 
-        const schools = ctx.query.name
-        ? await School.find({ 'name': { $regex: `${ctx.query.name}`, $options: 'i' } }).exec() 
-        : await School.find().exec()
+        let schools;
+        
+        if (ctx.query.name) {
+            schools = await School.find({ 'name': { $regex: new RegExp(`${ctx.query.name}`), $options: 'i' } }).exec();
+        } else {
+            schools = await School.find().exec();
+        }
 
         ctx.body = schools;
     },
