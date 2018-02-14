fa766a1597d3c2b3975780776115046a7bdff485
DSMdongly <gdh0608@naver.com>
2018-02-14 18:04:20 +0900

    fixed typo

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 const dotenv = require('dotenv');
 dotenv.config();
 
-const app = require('./app');
 const os = require('os');
 const http = require('http');
+const app = require('./app');
 
 app.listen(process.env.PORT, () => {
     console.log(`server was listening at ${process.env.PORT} port`);
