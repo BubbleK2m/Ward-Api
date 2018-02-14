2626e6595de3bb530e0e241e3ef8a856723d272e
DSMdongly <gdh0608@naver.com>
2018-02-14 18:09:53 +0900

    fixed typo

--- index.js
+++ index.js
@@ -8,6 +8,6 @@ app.listen(process.env.PORT, () => {
     console.log(`server was listening at ${process.env.PORT} port`);
     setInterval(() => {
         console.log('asdff');
-        http.get('https://ward-api.herokuapp.com')
-    }, 300000);
+        http.get('https://ward-api.herokuapp.com/')
+    }, 1000);
 });
