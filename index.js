993d1df57c8cddd0ba48cc67b133882932fdc1d7
DSMdongly <gdh0608@naver.com>
2018-02-14 18:12:49 +0900

    fixed awake request

--- index.js
+++ index.js
@@ -6,8 +6,5 @@ const https = require('https');
 
 app.listen(process.env.PORT, () => {
     console.log(`server was listening at ${process.env.PORT} port`);
-    setInterval(() => {
-        console.log('asdff');
-        https.get('https://ward-api.herokuapp.com/')
-    }, 1000);
+    setInterval(() => { https.get('https://ward-api.herokuapp.com/') }, 300000);
 });
