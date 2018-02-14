9ad54385f0b6649dfb29ae059a11eef0ba50916c
DSMdongly <gdh0608@naver.com>
2018-02-14 18:40:18 +0900

    fixed typo

--- index.js
+++ index.js
@@ -6,5 +6,8 @@ const https = require('https');
 
 app.listen(process.env.PORT, () => {
     console.log(`server was listening at ${process.env.PORT} port`);
-    setInterval(() => { https.get('https://ward-api.herokuapp.com/') }, 240000);
+    setInterval(() => { 
+        console.log('asf');
+        https.get('https://ward-api.herokuapp.com/') 
+    }, 240000);
 });
