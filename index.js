c88b722d680a0b71b42ad8115bdc43ef459e07a0
DSMdongly <gdh0608@naver.com>
2018-02-14 18:42:20 +0900

    fixed awake logic

--- index.js
+++ index.js
@@ -7,8 +7,5 @@ const https = require('https');
 app.listen(process.env.PORT, () => {
     console.log(`server was listening at ${process.env.PORT} port`);
     
-    setInterval(() => {
-        console.log('keep awake');
-        https.get('https://ward-api.herokuapp.com/') 
-    }, 240000);
+    setInterval(() => { https.get('https://ward-api.herokuapp.com/') }, 240000);
 });
